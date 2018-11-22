37aed6bb20cd384f018a6d0654e6bfba69ffb5ac
gmeunier <gmeunier@sigma.fr>
2018-11-22 11:09:25 +0100

    aki aki aki

--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import "./registerServiceWorker";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VuePouchDB from 'vue-pouch-db';
+import VueGoogleCharts from 'vue-google-charts'
 
+Vue.use(VueGoogleCharts)
 Vue.use(BootstrapVue);
+Vue.use(VuePouchDB);
 Vue.config.productionTip = false;
 
 new Vue({
