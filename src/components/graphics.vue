b0130ce5f93194c919c0f20436d524e10946f847
gmeunier <gmeunier@sigma.fr>
2018-11-30 10:52:25 +0100

    wololo

--- src/components/graphics.vue
+++ src/components/graphics.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="connection">
-    <GChart
-      type="ColumnChart"
-      :data="chartData"
-      :options="chartOptions"
-    />
-    <GChart
-      :settings="{ packages: ['table'] }"
-      type="Table"
-      :data="tableData"
-      :options="tableOptions"
-      :events="chartEvents"
-      ref="gChart"
-    />  
+    <GChart type="ColumnChart" :data="chartData" :options="chartOptions"/>
+    <GChart type="ColumnChart" :data="DaychartData" :options="DaychartOptions"/>
+    <b-container>
+      <b-row>
+        <b-col cols="5"></b-col>
+        <b-col cols="2">
+          <GChart
+            :settings="{ packages: ['table'] }"
+            type="Table"
+            :data="tableData"
+            :options="tableOptions"
+            :events="chartEvents"
+            ref="gChart"
+            id="tablepart"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import json from "../data/data.json"; 
+import json from "../data/data.json";
 
 export default {
   data() {
     return {
       chartData: [
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350]
+        ["type", "2015", "2016", "2017"],
+        ["congé", 25, 25, 25],
+        ["rtt", 12, 10, 4],
+        ["maladie", 5, 0, 20]
       ],
       chartOptions: {
         chart: {
@@ -35,22 +39,32 @@ export default {
           subtitle: "Sales, Expenses, and Profit: 2014-2017"
         }
       },
+      DaychartData: [
+        ["type", "2015", "2016", "2017"],
+        ["jour travaillé", 258, 253, 255]
+      ],
+      DaychartOptions: {
+        chart: {
+          title: "Company Performance",
+          subtitle: "Sales, Expenses, and Profit: 2014-2017"
+        }
+      },
       tableData: [
         [
-          { type: "string", label: "President", id: "President" },
-          { type: "date", label: "From", id: "From" },
-          { type: "date", label: "To", id: "To" }
+          { type: "string", label: "type", id: "type" },
+          { type: "string", label: "restant", id: "restant" },
+          { type: "string", label: "utilise", id: "utilise" }
         ],
-        ["Washington", new Date(1789, 3, 30), new Date(1797, 3, 4)],
-        ["Adams", new Date(1797, 2, 4), new Date(1802, 2, 4)],
-        ["Jefferson", new Date(1801, 2, 4), new Date(1809, 2, 4)]
+        ["rtt", "12", "2"],
+        ["vacances", "25", "25"],
+        ["congé maladie", "1", "1"]
       ],
       tableOptions: {
         chart: {
           title: "Company Performance",
           subtitle: "Sales, Expenses, and Profit: 2014-2017"
         }
-    }
+      }
     };
   }
 };
@@ -58,4 +72,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#tablepart {
+  width: 100%;
+}
 </style>
