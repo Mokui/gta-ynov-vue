fb8c5f33f5c958f3d4a5285e3357f101eacc2ad2
gmeunier <gmeunier@sigma.fr>
2018-11-30 10:21:17 +0100

    please work i have no time 4 this shit

--- src/components/graphics.vue
+++ src/components/graphics.vue
@@ -5,6 +5,14 @@
       :data="chartData"
       :options="chartOptions"
     />
+    <GChart
+      :settings="{ packages: ['table'] }"
+      type="Table"
+      :data="tableData"
+      :options="tableOptions"
+      :events="chartEvents"
+      ref="gChart"
+    />  
   </div>
 </template>
 
@@ -26,7 +34,23 @@ export default {
           title: "Company Performance",
           subtitle: "Sales, Expenses, and Profit: 2014-2017"
         }
-      }
+      },
+      tableData: [
+        [
+          { type: "string", label: "President", id: "President" },
+          { type: "date", label: "From", id: "From" },
+          { type: "date", label: "To", id: "To" }
+        ],
+        ["Washington", new Date(1789, 3, 30), new Date(1797, 3, 4)],
+        ["Adams", new Date(1797, 2, 4), new Date(1802, 2, 4)],
+        ["Jefferson", new Date(1801, 2, 4), new Date(1809, 2, 4)]
+      ],
+      tableOptions: {
+        chart: {
+          title: "Company Performance",
+          subtitle: "Sales, Expenses, and Profit: 2014-2017"
+        }
+    }
     };
   }
 };
